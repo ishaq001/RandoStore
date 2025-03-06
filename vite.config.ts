41258373073544d8b3be3afa8126e3bf3029cd25
ishaq <orakk2211@gmail.com>
2025-03-07 02:05:58 +0500

    add React Router React icons, Bootstrap & Toastify; configure Vite; clean up App.tsx & App.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      "/items": "http://localhost:3000",
+    },
+  },
+  build: {
+    outDir: "static",
+    emptyOutDir: true,
+  },
 })
